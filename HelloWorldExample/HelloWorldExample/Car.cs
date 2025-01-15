824217c2fb72e6a49de8c34f2cabe7c20ec0eda7
Conner Homan <connermhoman@gmail.com>
2025-01-14 19:45:24 -0500

    First version of Hello World
    A car object and a form

---
+++ HelloWorldExample/HelloWorldExample/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldExample
+{
+    public class Car
+    {
+        public string make;
+        public string model;
+        int year;
+        int month;
+        public string VIN;
+        public string exteriorColor;
+        public string interiorColor;
+        public Car(string make, string model, int year, int month, string vIN, string exteriorColor, string intereiorColor)
+        {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+            this.month = month;
+            this.VIN = vIN;
+            this.exteriorColor = exteriorColor;
+            this.interiorColor = intereiorColor;
+        }
+    }
+}
